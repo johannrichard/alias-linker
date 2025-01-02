2dd700e39a1e3a8a73b5420c4b5bf70deb16fea1
Johann Richard <189003+johannrichard@users.noreply.github.com>
2025-01-02 22:17:47 +0100

    perf: ⚡️ improve performance by optimizing search for alias

--- src/main.ts
+++ src/main.ts
@@ -7,29 +7,15 @@ import {
 } from "obsidian-typings";
 
 export default class ObsidianAutoLinkerPlugin extends Plugin {
-  aliasCache: LinkSuggestion[];
-  clearAliasCache = debounce(
-    () => {
-      this.aliasCache = [];
-    },
-    2000,
-    true
-  );
-
   patchMDCacheUninstaller: () => void;
 
   getFileByAlias(alias: string) {
-    // populate an emphemeral alias cache so that we don't slow this method down horribly
-    // the cache lives for 2 seconds before being cleared
-    this.populateAliasCache();
-    return this.aliasCache.find(
-      (a) => a.alias?.toLowerCase() === alias.toLowerCase()
-    );
+    const af = (a: LinkSuggestion) => a.alias?.toLowerCase() === alias.toLowerCase();
+    return this.app.metadataCache.getLinkSuggestions().find(af);
   }
 
   async onload() {
     const plugin = this;
-    this.aliasCache = [];
     this.patchMDCacheUninstaller = around(
       this.app.metadataCache.constructor.prototype,
       {
@@ -64,41 +50,4 @@ export default class ObsidianAutoLinkerPlugin extends Plugin {
   onunload(): void {
     this.patchMDCacheUninstaller();
   }
-
-  populateAliasCache() {
-    // on a vault of ~3k files, this takes around 3ms to populate
-    if (!this.aliasCache.length) {
-      this.aliasCache = this.app.metadataCache
-        .getLinkSuggestions()
-        .filter((file) => file.alias);
-    }
-    // Call this every time so that we keep resetting the debounced cache reset timer
-    // This means the cache will only reset after 2 seconds of getFirstLinkpathDest call inactivity
-    this.clearAliasCache();
-  }
-
-  refreshLinkResolverCache = () => {
-    // This will force a refresh of the link resolver cache
-    // Logic was borrowed from the default Obsidian MetadataCache.initialize() method
-    const cache: MetadataCache = this.app.metadataCache;
-    const metadataCache: MetadataCacheMetadataCacheRecord = cache.metadataCache;
-    const fileCache: MetadataCacheFileCacheRecord = cache.fileCache;
-    const markdownFiles: Record<string, TFile> = {};
-    const allLoadedFiles = this.app.vault.getAllLoadedFiles();
-
-    for (const file of allLoadedFiles) {
-      if (file instanceof TFile) {
-        cache.uniqueFileLookup.add(file.name.toLowerCase(), file);
-        markdownFiles[file.path] = file;
-      }
-    }
-
-    for (const filePath in fileCache) {
-      const markdownFile = markdownFiles[filePath];
-      const cacheEntry = fileCache[filePath];
-      if (markdownFile && metadataCache.hasOwnProperty(cacheEntry.hash)) {
-        cache.linkResolverQueue?.add(markdownFile);
-      }
-    }
-  };
 }
